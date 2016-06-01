74ea7756cfaffde82f7c0bfc20b23e7e816dbb6a
Austin Lyons <austin@fundify.com>
2016-06-01 13:32:20 -0500

    style a bit

--- src/Main.elm
+++ src/Main.elm
@@ -34,21 +34,26 @@ update msg model =
 -- CSS can be applied via class names or inline style attrib
 view : Model -> Html Msg
 view model =
-  div []
-    [ input [ type' "text", placeholder "Type verse here", onInput Attempt ] []
-    , viewValidation model
+  div [ class "container-fluid text-center main" ][
+    div [ class "row"][
+      div [ class "col-md-12" ][
+        h1 [][ text "Memory Verses" ],
+        textarea [ rows 4, cols 80, placeholder "Type verse here", onInput Attempt ] [],
+        viewValidation model
+      ]
     ]
+  ]
 
 viewValidation : Model -> Html msg
 viewValidation model =
   let
-    (color, message) =
+    (cls, message) =
       if String.contains model.attempt "In the beginning" then
-        ("green", "OK")
+        ("text-success", "OK")
       else
-        ("red", "Nope")
+        ("text-danger", "Nope")
   in
-    div [ style [("color", color)] ] [ text message ]
+    div [ class cls ] [ text message ]
 
 -- view model =
 --   div [ class "container", style [("margin-top", "30px"), ( "text-align", "center" )] ][    -- inline CSS (literal)
@@ -69,11 +74,11 @@ viewValidation model =
 
 
 -- CSS STYLES
-styles : { img : List ( String, String ) }
-styles =
-  {
-    img =
-      [ ( "width", "33%" )
-      , ( "border", "4px solid #337AB7")
-      ]
-  }
+-- styles : { img : List ( String, String ) }
+-- styles =
+--   {
+--     img =
+--       [ ( "width", "33%" )
+--       , ( "border", "4px solid #337AB7")
+--       ]
+--   }
