abddd4cba728da0bf76a0953b9ca3d2c8091e487
Austin Lyons <austin@fundify.com>
2016-06-06 11:24:16 -0500

    Clear input on next/back

--- src/Main.elm
+++ src/Main.elm
@@ -51,18 +51,23 @@ update : Msg -> Model -> Model
 update msg model =
   let
     len = List.length versesList
-    _ = Debug.log "model" model.idx -- write to console.log
+    -- _ = Debug.log "model" model.idx -- write to console.log
   in
     case msg of
       Attempt attempt ->
         { model | attempt = attempt }
 
       Next ->
-        { model | idx = (model.idx + 1) % len }
+        { model |
+            idx = (model.idx + 1) % len,
+            attempt = "" -- clear textarea
+        }
 
       Back ->
-        { model | idx = (model.idx - 1) % len }
-
+        { model |
+            idx = (model.idx - 1) % len,
+            attempt = ""
+        }
 
 -- VIEW
 -- Html is defined as: elem [ attribs ][ children ]
@@ -77,7 +82,7 @@ view model =
         div [ class "col-md-12" ][
           h1 [][ text "Memory Verses" ],
           h3 [][ text currentVerse ],
-          textarea [ rows 4, cols 80, placeholder "Type verse here", onInput Attempt ] [],
+          textarea [ rows 4, cols 80, placeholder "Type verse here",  value model.attempt, onInput Attempt ] [],
           viewValidation model,
           div [] [
             button [ onClick Back ] [ text "back" ],
@@ -90,41 +95,14 @@ view model =
 viewValidation : Model -> Html msg
 viewValidation model =
   let
-    txt = getEntryText (Dict.get model.idx verses)
+    actual = String.toLower (getEntryText (Dict.get model.idx verses))
+    attempt = String.toLower model.attempt
     (cls, message) =
-      if model.attempt == txt then
-        ("text-success", "Correct!!")
-      else if String.contains model.attempt txt then
-        ("text-ok", "OK so far ...")
+      if attempt == actual then
+        ("status text-success", "Correct!!")
+      else if String.startsWith attempt actual then
+        ("status text-ok", "OK so far ...")
       else
-        ("text-danger", "Incorrect")
+        ("status text-danger", "Incorrect")
   in
-    div [ class cls ] [ text message]
-
--- view model =
---   div [ class "container", style [("margin-top", "30px"), ( "text-align", "center" )] ][    -- inline CSS (literal)
---     div [ class "row" ][
---       div [ class "col-xs-12" ][
---         div [ class "jumbotron" ][
---           img [ src "img/elm.jpg", style styles.img ] []                                    -- inline CSS (via var)
---           , hello model                                                                     -- ext 'hello' component (takes 'model' as arg)
---           , p [] [ text ( "Elm Webpack Starter" ) ]
---           , button [ class "btn btn-primary btn-lg", onClick Increment ] [                  -- click handler
---             span[ class "glyphicon glyphicon-star" ][]                                      -- glyphicon
---             , span[][ text "FTW!" ]
---           ]
---         ]
---       ]
---     ]
---   ]
-
-
--- CSS STYLES
--- styles : { img : List ( String, String ) }
--- styles =
---   {
---     img =
---       [ ( "width", "33%" )
---       , ( "border", "4px solid #337AB7")
---       ]
---   }
+    div [ class cls ] [ text message ]
